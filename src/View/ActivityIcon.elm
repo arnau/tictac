05825662a98a30dab85e88eb8fb6a0aa4bc4f5d8
Arnau Siches <asiches@gmail.com>
2017-08-28 13:36:26 +0100

    Add activity icon title
    Signed-off-by: Arnau Siches <asiches@gmail.com>

--- src/View/ActivityIcon.elm
+++ src/View/ActivityIcon.elm
@@ -24,5 +24,6 @@ icon color =
         , strokeLinecap "round"
         , strokeLinejoin "round"
         ]
-        [ polyline [ points "22 12 18 12 15 21 9 3 6 12 2 12" ] []
+        [ Svg.title [] [ text "Activity" ]
+        , polyline [ points "22 12 18 12 15 21 9 3 6 12 2 12" ] []
         ]
